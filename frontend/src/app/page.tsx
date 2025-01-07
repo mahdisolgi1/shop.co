9a17919d0b03a90094ee482fbb85c3f51c18728d
MahdiSolgi <vass1383@gmail.com>
2025-01-07 11:39:05 -0800

    feat(page): add NewArrivals component to the Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import HeroSection from "../components/HeroSection";
+import NewArrivals from "../components/NewArrivals";
 
 const page: FC = () => {
   return (
     <>
       <HeroSection />
+      <NewArrivals />
     </>
   );
 };
